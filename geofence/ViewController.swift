6517b694f28bb3fd72ee0ff5c41da18014728a5c
Zaid M. Said <zaid.shr@gmail.com>
2019-12-02 13:58:33 +0800

    initial commit

---
+++ geofence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  geofence
+//
+//  Created by Zaid Said on 02/12/2019.
+//  Copyright © 2019 Zaid M. Said. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
